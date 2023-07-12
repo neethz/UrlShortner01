dabe3e505021a7dff50e102f340885cd02a818fa
jeffi <neethu.bc89@gmail.com>
2023-07-12 12:42:12 +0530

    first commit

---
+++ src/main/java/com/neethu/urlshortener01/UrlShortener01Application.java
@@ -0,0 +1,13 @@
+package com.neethu.urlshortener01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortener01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortener01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/neethu/urlshortener01/UrlShortener01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neethu.urlshortener01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortener01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
